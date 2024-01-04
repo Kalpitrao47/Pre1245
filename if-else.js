dd4a6e4aff756635a4d164f2ff0265e6a7fd27f1
unknown <kalpitraorane47@gmail.com>
2024-01-04 17:50:41 +0530

    if-else

---
+++ if-else.js
@@ -0,0 +1,42 @@
+if (condition) {
+    // Code to be executed if the condition is true
+  } else {
+    // Code to be executed if the condition is false
+  }
+  
+
+
+
+const age = 18;
+
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote yet.");
+}
+
+
+const score = 75;
+
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else if (score >= 70) {
+  console.log("C");
+} else {
+  console.log("D");
+}
+
+
+
+const temperature = 25;
+
+if (temperature > 30) {
+  console.log("It's hot outside!");
+}
+
+
+
+
+

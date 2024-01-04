4ab85c52c78c9f7832f75094764d774d3facf5ec
unknown <kalpitraorane47@gmail.com>
2024-01-04 17:59:29 +0530

    TernaryOperator

---
+++ TernaryOperator.js
@@ -0,0 +1,21 @@
+//1-condition ? expressionIfTrue : expressionIfFalse;
+
+
+//2-
+ const age = 20;
+
+const message = age >= 18 ? "You are an adult" : "You are not an adult";
+
+console.log(message);
+// Output: "You are an adult"
+
+
+
+
+//3-
+const number = 5;
+
+const result = number % 2 === 0 ? "even" : "odd";
+
+console.log(result);
+// Output: "odd"
